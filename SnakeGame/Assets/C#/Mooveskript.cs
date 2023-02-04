6666c63aeb8b7a869c62731c842c044d63e202b4
WIRIT <118571958+eWIRITe@users.noreply.github.com>
2023-02-04 16:38:14 -0500

    Add game

---
+++ SnakeGame/Assets/C#/Mooveskript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Mooveskript : MonoBehaviour
+{
+    public Rigidbody _rb;
+
+    public float Speed;
+
+    public KeyCode KeyForBeginMoove;
+
+    public bool isCanMoove;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyForBeginMoove)) 
+        { 
+            isCanMoove = !isCanMoove;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(isCanMoove) _rb.AddRelativeForce(new Vector3(0, 0, Speed));
+    }
+}
