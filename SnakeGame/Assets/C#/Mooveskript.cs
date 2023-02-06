832281ee2d029d165f8d1a86fee3c6947f454192
WIRIT <118571958+eWIRITe@users.noreply.github.com>
2023-02-05 19:11:55 -0500

    add new mehanick

--- SnakeGame/Assets/C#/Mooveskript.cs
+++ SnakeGame/Assets/C#/Mooveskript.cs
@@ -2,29 +2,112 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Mirror;
+using Cinemachine;
 
-public class Mooveskript : MonoBehaviour
+public class Mooveskript : NetworkBehaviour
 {
-    public Rigidbody _rb;
+    //list of body pices
+    public List<Transform> BodyPices = new List<Transform>();
 
+    //Moove varibles
+    public float MinDistance;
+    public float RotationSpeed;
     public float Speed;
-
-    public KeyCode KeyForBeginMoove;
-
     public bool isCanMoove;
 
-    // Update is called once per frame
-    void Update()
+    public float BeginSize;
+
+    //Key coodes
+    public KeyCode keyForSprint;
+
+    //Prefab of body pices
+    public GameObject BodyPicePref;
+
+    private float Dis;
+    private Transform curBodyPart;
+    private Transform PrevBodypart;
+
+    //Camera
+    public CinemachineVirtualCamera _virtualCamera;
+    public string nameOfVirtualCamera;
+    public GameObject head;
+
+
+    public void Start()
     {
-        if(Input.GetKeyDown(KeyForBeginMoove)) 
-        { 
-            isCanMoove = !isCanMoove;
+        if (!isLocalPlayer) { return; }
+
+        _virtualCamera = GameObject.Find(nameOfVirtualCamera).GetComponent<CinemachineVirtualCamera>();
+        _virtualCamera.Follow = head.transform;
+        _virtualCamera.LookAt = head.transform;
+
+        for (int i = 0; i < BeginSize - 1; i++)
+        {
+            AddBodyPice();
         }
     }
 
-    void FixedUpdate()
+    public void Update()
     {
-        if (isCanMoove) _rb.AddRelativeForce(new Vector3(0, 0, Speed));
-        Speed += 0.01f;
+        if (!isLocalPlayer) { return; }
+        //function moove
+        Moove();
     }
+
+    public void Moove()
+    {
+        float _speed = Speed;
+
+        //if key to stint pressed multiply speed by 2
+        if (Input.GetKey(keyForSprint))    _speed *= 2;
+        
+        //Moove first pice
+        BodyPices[0].Translate(BodyPices[0].forward * _speed * Time.smoothDeltaTime, Space.World);
+
+        //Rotate first pice
+        if(Input.GetAxis("Horizontal") != 0)
+            BodyPices[0].Rotate(Vector3.up * RotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+
+        for (int i = 1; i < BodyPices.Count; i++)
+        {
+            //tace pice
+            curBodyPart = BodyPices[i];
+            //take last pice
+            PrevBodypart = BodyPices[i-1];
+
+            //count distance
+            Dis = Vector3.Distance(PrevBodypart.position, curBodyPart.position);
+            
+            //create new Pos 
+            Vector3 newpos = PrevBodypart.position;
+
+            //set new Pos y 
+            newpos.y = BodyPices[0].position.y;
+
+            //cound able dostance
+            float T = Time.deltaTime * Dis / MinDistance * _speed;
+
+            //if distance to long, we cut it
+            if (T > 0.5)
+                T = 0.5f;
+
+            //moove pice
+            curBodyPart.position = Vector3.Slerp(curBodyPart.position, newpos, T);
+            curBodyPart.rotation = Quaternion.Slerp(curBodyPart.rotation, PrevBodypart.rotation, T);
+        }
+    }
+
+    public void AddBodyPice()
+    {
+        //creating new body pice
+        Transform newPice = (Instantiate(BodyPicePref, BodyPices[BodyPices.Count - 1].position, BodyPices[BodyPices.Count - 1].rotation) as GameObject).transform;
+    
+        //set parant
+        newPice.SetParent(transform);
+    
+        //add our new body pice in BodyPices list
+        BodyPices.Add(newPice);
+    }
+
 }
---
+++ SnakeGame/Assets/C#/TriggerSnakeScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSnakeScript : MonoBehaviour
+{
+    public string TagOfBlocks;
+    public GameObject Player;
+
+    //varebles for dead
+    public GameObject DeadCanv;
+
+    //varebles for Bonus
+    public string TagOfBonus;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //when we touth something, we start function "Dead"
+
+        if(other.tag == TagOfBlocks || other.tag == "Player")
+        {
+            Dead();
+        }
+        if(other.tag == TagOfBonus)
+        {
+            Player.GetComponent<Mooveskript>().AddBodyPice();
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void Dead()
+    {
+        DeadCanv.SetActive(true);
+    }
+}
